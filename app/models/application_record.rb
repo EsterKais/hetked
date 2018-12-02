c56536d5eed101627f901b1967fe2c9ee578f2ae
Ester Kais <e.kais@youngcapital.nl>
2018-12-02 11:37:29 +0100

    Basic/Temporary user model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  before_save :generate_uuid
+  before_save :normalize_blank_values
+
+  UUID_REGEX = /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/
+
+  def generate_uuid
+    self.id ||= SecureRandom.uuid
+  end
+
+  def normalize_blank_values
+    attributes.each { |column, _| self[column] = self[column].presence }
+  end
 end
---
+++ db/migrate/20181202103328_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users, id: false do |t|
+      t.string :id, limit: 36, null: false, primary_key: true
+      t.string :email, null: false
+
+      t.index :email, unique: true
+
+      t.timestamps
+    end
+  end
+end
